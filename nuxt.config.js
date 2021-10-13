dddf625b6edf921a7935d5b257d291dd56165fa8
Pierson <pro.pierson.alban@gmail.com>
2021-10-13 16:51:55 +0200

    Add typos

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['normalize.css/normalize.css'],
+  css: [
+    '@/assets/general.css',
+    'normalize.css/normalize.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
